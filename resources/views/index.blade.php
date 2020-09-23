562d4b0e1417a1cc9b0c26a493f85c9cab17f1f0
Dan Norris <dan.norris@hotmail.com>
2020-09-23 19:36:09 +0100

    styled basic index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-blah blah
+    <div class="mx-auto container mt-8">
+
+        <h1 class="text-gray-300 font-bold text-lg uppercase leading-none border-l-4 border-blue-500 pl-4">Highest Sparkle Rating</h1>
+
+        <div class="grid grid-cols-6 grid-rows-2 gap-4 mt-8">
+            @foreach (range(1, 12) as $movie)
+            <div class="flex-col">
+                <div class="bg-gray-400 rounded w-full h-64"></div>
+                <div class="flex p-2 space-x-2">
+                    <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                    </svg>
+                    <span>92%</span>
+                </div>
+                <p class="p-2">Avatar</p>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,21 +9,29 @@
 
 </head>
 
-<body class="bg-gray-900 text-white">
-    <header class="flex justify-between mx-6 p-6">
-        <h1 class="text-gray-300 font-bold text-2xl">Flicks</h1>
+<body class="bg-gray-800 text-white">
+    <header class="flex justify-between px-8 py-6 border-t-4 border-blue-500 bg-gray-900 shadow-md">
+        <a href="/">
+            <h1 class="text-gray-300 font-bold text-2xl uppercase leading-none border-l-4 border-blue-500 pl-4">Flicks</h1>
+        </a>
         <nav>
-            <ul class="text-sm flex space-x-4">
-                <a href="#">
-                    <li class="hover:text-gray-400">Top 50</li>
+            <ul class="text-sm flex space-x-8">
+                <a href="#" class="hover:text-gray-400 flex items-center">
+                    <svg class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14" />
+                      </svg>
+                    <li>Top 50</li>
                 </a>
-                <a href="#" class="hover:text-gray-400">
+                <a href="#" class="hover:text-gray-400 flex items-center">
+                    <svg class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
+                      </svg>
                     <li>Movies</li>
                 </a>
-                <a href="#" class="hover:text-gray-400">
-                    <li>TV Series</li>
-                </a>
-                <a href="#" class="hover:text-gray-400">
+                <a href="#" class="hover:text-gray-400 flex items-center">
+                    <svg class="w-6 h-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                      </svg>
                     <li>Login</li>
                 </a>
             </ul>
@@ -34,7 +42,7 @@
         @yield('content')
     </main>
 
-    <footer class="mx-6 p-6 text-sm">
+    <footer class="px-8 py-6 text-sm bg-gray-900 mt-8">
         Powered by the <a href="http://www.omdbapi.com/" class="underline">OMDb API</a>
     </footer>
 
