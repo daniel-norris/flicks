fdeaf3973d2889445a7ababa08583f8e6ab2c3d0
Dan Norris <dan.norris@hotmail.com>
2020-09-23 17:59:24 +0100

    created a header and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,17 +5,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Flicks</title>
+    <link href="/css/main.css" rel="stylesheet">
+
 </head>
 
-<body>
-
-    @section('nav')
-
-    @endsection
+<body class="bg-gray-900 text-white">
+    <header class="flex justify-between mx-6 p-6">
+        <h1 class="text-gray-300 font-bold text-2xl">Flicks</h1>
+        <nav>
+            <ul class="text-sm flex space-x-4">
+                <a href="#">
+                    <li class="hover:text-gray-400">Top 50</li>
+                </a>
+                <a href="#" class="hover:text-gray-400">
+                    <li>Movies</li>
+                </a>
+                <a href="#" class="hover:text-gray-400">
+                    <li>TV Series</li>
+                </a>
+                <a href="#" class="hover:text-gray-400">
+                    <li>Login</li>
+                </a>
+            </ul>
+        </nav>
+    </header>
 
     <main>
         @yield('content')
     </main>
+
+    <footer class="mx-6 p-6 text-sm">
+        Powered by the <a href="http://www.omdbapi.com/" class="underline">OMDb API</a>
+    </footer>
 
 </body>
 
