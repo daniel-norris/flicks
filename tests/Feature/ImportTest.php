1fc2ba2c95778ee81fd89ab2347eb6a1b1f7234a
Dan Norris <dan.norris@hotmail.com>
2020-10-10 20:14:32 +0100

    created feature test for data dump unzip

--- tests/Feature/ImportTest.php
+++ tests/Feature/ImportTest.php
@@ -22,18 +22,29 @@ class ImportTest extends TestCase
         $client->get('http://files.tmdb.org/p/exports/movie_ids_09_09_2020.json.gz', ['save_to' => storage_path() . '/app/imports/testFile.json.gz']);
 
         Storage::disk('local')->assertExists('/imports/testFile.json.gz');
+        // Storage::disk('local')->delete('/imports/testFile.json.gz');
         Storage::disk('local')->assertMissing('/imports/testFile.json');;
     }
 
-    public function testMovieDataDumpUnzip()
+    public function testDumpUnzip()
     {
-        $filename = Storage::get('/imports/testFile.json.gz');
-        $file = gzfile($filename);
-        print_r($filename);
+        $file_name = 'storage/app/imports/testFile.json.gz';
 
-        $lines = gzfile($filename);
-        foreach ($lines as $line) {
-            echo $line;
+        $buffer_size = 4096;
+        $out_file_name = str_replace('.gz', '', $file_name);
+
+        $file = gzopen($file_name, 'rb');
+        $out_file = fopen($out_file_name, 'wb');
+
+        while (!gzeof($file)) {
+            fwrite($out_file, gzread($file, $buffer_size));
         }
+
+        fclose($out_file);
+        gzclose($file);
+
+        Storage::disk('local')->assertExists('/imports/testFile.json');
+        Storage::disk('local')->delete('/imports/testFile.json');
+        Storage::disk('local')->assertMissing('/imports/testFile.json');
     }
 }
