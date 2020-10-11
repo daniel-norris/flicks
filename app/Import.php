b91b39fee62e38736c7e6a3439803a97d883bfc5
Dan Norris <dan.norris@hotmail.com>
2020-10-11 19:04:14 +0100

    created an unzip method on the import model

--- app/Import.php
+++ app/Import.php
@@ -3,8 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Import extends Model
 {
     protected $guarded = [];
+
+    public function unzip(string $path): string
+    {
+        $filename = $path;
+
+        $bufferSize = 4096;
+        $outputFilename = str_replace('.gz', '', $filename);
+
+        $file = gzopen($filename, 'rb');
+        $output = fopen($outputFilename, 'wb');
+
+        while (!gzeof($file)) {
+            fwrite($output, gzread($file, $bufferSize));
+        }
+
+        fclose($output);
+        gzclose($file);
+
+        // return Storage::url('/imports/testFile.json');
+    }
 }
