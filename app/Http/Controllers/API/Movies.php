14eabaf66e4a168f7b183022766cc957cba7e0e7
Dan Norris <dan.norris@hotmail.com>
2020-10-29 01:02:07 +0000

    created a get movie endpoint

--- app/Http/Controllers/API/Movies.php
+++ app/Http/Controllers/API/Movies.php
@@ -35,9 +35,9 @@ class Movies extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Movie $id)
     {
-        //
+        return $id;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get("/movies", [Movies::class, "index"]);
+
+Route::get("/movie/{id}", [Movies::class, "show"]);
